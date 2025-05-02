71d8f786bd04e74ff0591fcdec63bab3b78083df
David Gasquez <davidgasquez@gmail.com>
2025-05-02 11:52:27 +0200

    🎨 Update Astro configuration and styles, enhance font integration, and remove unused headers

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,25 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
 import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://datonic.io",
   compressHTML: true,
-
+  trailingSlash: "never",
+  build: {
+    format: "file",
+  },
+  experimental: {
+    clientPrerender: true,
+    fonts: [{
+      provider: fontProviders.bunny(),
+      name: "JetBrains Mono",
+      cssVariable: "--font-monospace"
+    }]
+  },
   prefetch: {
-      prefetchAll: true,
+    prefetchAll: true,
   },
 
   integrations: [sitemap()],
